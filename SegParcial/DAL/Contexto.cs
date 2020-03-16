32ee943236d49591a245e8a7d1d8e88eaabcd144
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2020-03-16 16:29:50 -0400

    Add project files.

---
+++ SegParcial/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source: Database\SegParcial.db");
+        }
+
+       
+    }
+}
